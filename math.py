ff8a3f92a045963cdc08b76190e17b9606701261
HimadriBhattacharya <32800883+HimadriBhattacharya@users.noreply.github.com>
2021-06-03 14:39:08 +0530

    Innitial commit of math.py

---
+++ math.py
@@ -0,0 +1,15 @@
+#Add
+def add(x,y):
+    return x+y; #add
+	#subtract
+def subtract(x,y):
+    return x-y   #subtract
+	#multiply
+def multiply(x,y):
+    return x*y  #multiply
+
+def divide(x,y):
+    return (x/y)
+#square
+def square(x)
+	pass
